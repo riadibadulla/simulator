01268fb5f77304b7f0f65934f012ba6e1aea7736
Ibadulla <Riad.Ibadulla@city.ac.uk>
2021-11-18 14:43:45 +0000

    any size matmult, except the num_rows_mat1 should be 2

--- Electro.py
+++ Electro.py
@@ -2,31 +2,38 @@ import numpy as np
 import scipy
 from scipy.signal import convolve2d
 
-def conv_matmul(vector, matrix):
-    vector = np.rot90(vector,1, axes=(0,1))
-    result = convolve2d(vector,matrix)
-    result = np.array([result[len(result)//2]])
-    print(result)
 
 # sample_vector = np.array([[2,3,4]])
 # matrix = np.array([[5,3,5],[4,2,1],[1,5,6]])
-# print(np.matmul(sample_vector,matrix))
-# conv_matmul(sample_vector, matrix)
 #
+# def conv_matmul(vector, matrix):
+#     vector = np.rot90(vector,1, axes=(0,1))
+#     result = scipy.signal.fftconvolve(vector,matrix)
+#     result = np.array([result[len(result)//2]])
+#     return result
+#
+#
+# print(np.matmul(sample_vector,matrix))
+# print(conv_matmul(sample_vector, matrix))
 
-def matmult(matrix1,matrix2):
+
+# mat1 = np.array([[1,2],[1,4]])
+# mat2 = np.array([[2,-1],[-5,2]])
+
+mat1 = np.array([[1,2,5],[1,4,5]])
+mat2 = np.array([[2,-1,2],[-5,2,1],[-5,2,1]])
+
+def conv_matmult(matrix1,matrix2):
     results_size_x = matrix1.shape[0]
-    results_size_y = matrix1.shape[1]
+    results_size_y = matrix2.shape[1]
+    size_of_padding = results_size_y-results_size_x+1
     matrix1 = np.rot90(matrix1,1, axes=(0,-1))
-    matrix1 = np.insert(matrix1, 1, values=0, axis=1)
-    result = convolve2d(matrix1, matrix2)
+    for i in range(size_of_padding):
+        matrix1 = np.insert(matrix1, 1, values=0, axis=1)
+    result = scipy.signal.fftconvolve(matrix1, matrix2)
     result = np.array([result[len(result) // 2]])
     result = result.reshape((results_size_x,results_size_y))
-    print(result)
+    return result
 
-
-mat1 = [[1,2],[1,4]]
-mat2 = [[2,-1],[-5,2]]
-mat1 = np.array(mat1)
-mat2 = np.array(mat2)
-matmult(mat1,mat2)
+print(np.matmul(mat1,mat2))
+print(conv_matmult(mat1,mat2))
