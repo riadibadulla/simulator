02e34527f521ae357b67acda1a8e88b7f8add7fa
Riad <riadibadulla@gmail.com>
2022-02-12 16:46:56 +0000

    using opitcs in the conv simulation

--- Optics_simulation.py
+++ Optics_simulation.py
@@ -8,11 +8,12 @@ class Optics_simulation:
     def __init__(self):
         self.wavelength = 500 * u.nm
         self.pixel_scale = 10 * u.um
-        self.npix = 300
+        self.npix = 450
+        # 300
         self.na = 0.35
         self.coherence_factor = 0
 
-        self.f = 6 * u.cm
+        self.f = 9 * u.cm
         self.axis_unit = u.mm
         self.wf = po.Wavefront(self.wavelength, self.pixel_scale, self.npix)
         self.r = 2 * u.mm
@@ -22,11 +23,6 @@ class Optics_simulation:
     def rgb2gray(self, rgb):
         img = np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
         return img
-
-    def modulate_signal(self, image_dir):
-        img = plt.imread(image_dir)
-        target = self.rgb2gray(img)
-        self.wf.amplitude = target
 
     def plot_wavefront(self, wavefront, amplitude):
         fig = wavefront.plot(amplitude=amplitude, fig_options=dict(figsize=(5, 5), dpi=130))
@@ -38,24 +34,16 @@ class Optics_simulation:
     def make_fourier_engine(self, waveform):
         return waveform * self.fs * self.lens * self.fs
 
-    def no_convolution_4F(self, input_image_link):
-        print(self.fs.calc_optimal_distance(self.wf))
-        self.modulate_signal(input_image_link)
-        self.plot_wavefront(self.wf, dict(vmax=1))
-        # print(fs.calc_optimal_distance(wf))
+    def no_convolution_4F(self, img):
+        self.wf.amplitude = img
         wf_imaged = self.make_4F_engine(self.wf)
-        print(type(wf_imaged))
-        self.plot_wavefront(wf_imaged, dict(vmax=1))
+        return wf_imaged.amplitude
 
-    def convolution_4F(self, input_image_link,initial_kernel):
-        self.modulate_signal(input_image_link)
-        self.plot_wavefront(self.wf, dict(vmax=1))
+    def convolution_4F(self, img, kernel):
+        self.wf.amplitude = img
         wf_in_frequency_domain = self.make_fourier_engine(self.wf)
-        self.plot_wavefront(wf_in_frequency_domain, dict(vmax=1))
-        convoluted_matrix = np.multiply(wf_in_frequency_domain.wavefront, initial_kernel)
-        # print(convoluted_matrix)
+        convoluted_matrix = np.multiply(wf_in_frequency_domain.wavefront, kernel)
         self.wf.wavefront = convoluted_matrix
-        self.plot_wavefront(self.wf, dict(vmax=1))
         wf_imaged = self.make_fourier_engine(self.wf)
         self.plot_wavefront(wf_imaged, "default")
         return wf_imaged.amplitude
--- Run.py
+++ Run.py
@@ -8,8 +8,6 @@ from skimage.color import rgb2gray
 from skimage.transform import resize
 
 
-
-
 def kernel_in_frequency_domain(kernel):
     padded_kernel = np.pad(np.array(kernel), 223)
     kernel_fr = np.fft.fftshift(np.fft.fft2(padded_kernel))
@@ -33,43 +31,35 @@ def fft_based_convolution(img, sample_kernel):
         if padded_image.shape != sample_kernel.shape:
             padded_image = np.pad(padded_image, ((0,1),(0,1)))
 
-    #fft of image
-    signal_fr = np.fft.fftshift(np.fft.fft2(img))
-    plt.imshow(np.log(abs(signal_fr)), cmap='gray')
-    plt.title("Image FFT")
-    plt.show()
-
     #kernel fft
     kernel_fr = np.fft.fftshift(np.fft.fft2(padded_kernel))
-    plt.imshow(abs(kernel_fr), cmap='gray')
-    plt.title("Kernel FFT")
-    plt.show()
-
-
-    result = np.fft.fftshift(np.fft.ifft2(np.multiply(signal_fr, kernel_fr)))
-    plt.imshow(abs(result), cmap='gray')
-    plt.colorbar()
-    plt.title("FFTConvolved")
-    plt.show()
-    return abs(result)
+    optics = Optics_simulation()
+    result = optics.convolution_4F(img,kernel_fr)
+    result = np.fft.fftshift(result)
+    return result
 
 
 def optConv2d(img,kernel,pseudo_negativity=True):
+    optics = Optics_simulation()
     if pseudo_negativity:
         pos, neg = np.maximum(kernel, 0), np.maximum(kernel * (-1), 0)
         output_pos = fft_based_convolution(INPUT_IMAGE, pos)
+        output_pos = optics.no_convolution_4F(output_pos)
         output_neg = fft_based_convolution(INPUT_IMAGE, neg)
+        optics = Optics_simulation()
+        output_neg = optics.no_convolution_4F(output_neg)
         output_fin = output_pos - output_neg
     else:
         output_fin = fft_based_convolution(INPUT_IMAGE, kernel)
 
+    print(output_fin)
     plt.imshow(output_fin)
     plt.show()
-
+    print(signal.convolve2d(img,kernel))
     plt.imshow(signal.convolve2d(img,kernel))
     plt.show()
 
 if __name__=='__main__':
-    kernel = np.random.random(size=(7, 7)) - 0.5
+    kernel = np.random.random(size=(7, 7))-0.5
     INPUT_IMAGE = resize(rgb2gray(data.chelsea()), (450, 450)) / 255
     optConv2d(INPUT_IMAGE,kernel,True)
