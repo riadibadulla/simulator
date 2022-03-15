Fixed the rotation bug

Optics_simulation.py
@@ -103,7 +103,7 @@ class Optics_simulation:
         else:
             result = self.convolution_4F(img, kernel)
         result = torch.fft.fftshift(result)
-        # result = self.no_convolution_4F(result)
+        result = self.no_convolution_4F(result)
         return result
 
 