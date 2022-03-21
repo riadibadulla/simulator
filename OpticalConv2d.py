fixed kernel init, pseudo-neg set to False, no rotation is performed anymore

OpticalConv2d.py
@@ -12,13 +12,14 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 class OpticalConv2dNew(nn.Module):
 
-    def __init__(self,input_channels,output_channels,kernel_size):
+    def __init__(self,input_channels,output_channels,kernel_size,pseudo_negativity=False):
         super().__init__()
+        self.pseudo_negativity = pseudo_negativity
         self.input_channels, self.output_channels = input_channels, output_channels
         self.kernel_size = kernel_size
         kernel = torch.Tensor(output_channels,input_channels,kernel_size,kernel_size)
         self.kernel = nn.Parameter(kernel)
-        nn.init.kaiming_uniform_(self.kernel, a=math.sqrt(5))
+        nn.init.kaiming_uniform_(self.kernel)
 
     def forward(self,input):
         opt = Optics_simulation(input.shape[2])
@@ -28,7 +29,7 @@ class OpticalConv2dNew(nn.Module):
                 for image in input[batch, :, :, :]:
                     input_channel = 0
                     output[batch, output_channel, :, :] = output[batch, output_channel, :, :] + opt.optConv2d(
-                        image, self.kernel[output_channel, input_channel, :, :])
+                        image, self.kernel[output_channel, input_channel, :, :], pseudo_negativity=self.pseudo_negativity)
                     input_channel += 1
         del opt
         return output

Optics_simulation.py
@@ -92,7 +92,7 @@ class Optics_simulation:
         return img, kernel
 
 
-    def optConv2d(self, img,kernel,pseudo_negativity=True):
+    def optConv2d(self, img,kernel,pseudo_negativity=False):
         img, kernel = self.process_inputs(img, kernel)
         if pseudo_negativity:
             relu = ReLU()
@@ -106,7 +106,7 @@ class Optics_simulation:
         else:
             result = self.convolution_4F(img, kernel)
         result = torch.fft.fftshift(result)
-        result = self.no_convolution_4F(result)
+        # result = self.no_convolution_4F(result)
         return result
 
 

Run.py
@@ -80,7 +80,7 @@ def train():
                 correct += (predicted == labels.detach().cpu()).sum().item()
                 running_accuarcy = 100 * correct // total
             del inputs, labels, loss, outputs
-            if i%20==0:
+            if i%5==0:
                 print(running_loss)
                 print(running_accuarcy)
             torch.cuda.empty_cache()
@@ -104,6 +104,7 @@ if __name__=='__main__':
     # writer.add_graph(net, images)
     # writer.flush()
     # writer.close()
+    # quit()
     net.to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.0001, momentum=0.9)