a0dfad039c0f6ac3aae306c304a7fac91b388d17
Ibadulla <Riad.Ibadulla@city.ac.uk>
2022-08-22 14:15:20 +0100

    simplified the utils file

--- Optics/utils.py
+++ Optics/utils.py
@@ -1,7 +1,3 @@
-import logging
-import warnings
-
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
@@ -15,14 +11,6 @@ def fft(array):
     :type method: str
     :return: Fourier transformed array
     :rtype: np.array
-
-    **Example**
-
-    >>> import pyoptica as po
-    >>>
-    >>> arr = np.ones((1024, 1024))
-    >>> po.utils.fft(arr)
-
     """
     return torch.fft.fft2(array)
 
@@ -36,14 +24,6 @@ def ifft(array: np.array) -> np.array:
     :type method: str
     :return: Fourier transformed array
     :rtype: np.array
-
-    **Example**
-
-    >>> import pyoptica as po
-    >>>
-    >>> arr = np.ones((1024, 1024))
-    >>> po.utils.ifft(arr)
-
     """
     return torch.fft.ifft2(array)
 
@@ -58,36 +38,9 @@ def mesh_grid(npix, pixel_scale):
     :type pixel_scale: astropy.Quantity (type is not forced!)
     :return: two dimensional mesh grid (x, y)
     :rtype: (np.array, np.array)
-
-    **Example**
-
-    >>> import astropy.units as u
-    >>> import pyoptica as po
-    >>>
-    >>> pixel_scale = 1 * u.um
-    >>> npix = 512
-    >>> x, y = po.utils.mesh_grid(npix, pixel_scale)
-
     """
     y, x = (np.indices((npix, npix)) - npix / 2) * pixel_scale
-    # return torch.tensor(x), torch.tensor(y)
     return x,y
 
 
-def rgb2gray(rgb):
-    """Converts rgb(a) image to grayscale. gray = 0.299r + 0.587g + 0.114b
-    Based on: https://en.wikipedia.org/wiki/Grayscale
-
-    :param rgb: array representing an image rgb(a) of size (n, m, 3/(4))
-    :type: np.array
-    :return: grayscale image of size (n, m)
-    :rtype: np.array
-
-    **Example**
-
-    >>> import pyoptica as po
-    >>> rgb = np.ones((16, 16, 3))
-    >>> po.utils.rgb2gray(rgb)
-    """
-    return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
 
